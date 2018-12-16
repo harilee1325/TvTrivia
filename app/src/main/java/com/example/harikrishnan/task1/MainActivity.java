fc4bdb3f25c98a868815c70cbad4f8a256117aff
harilee1325 <45534309+harilee1325@users.noreply.github.com>
2018-12-16 17:54:19 +0530

    completed

--- app/src/main/java/com/example/harikrishnan/task1/MainActivity.java
+++ app/src/main/java/com/example/harikrishnan/task1/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.harikrishnan.task1;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Movie> catDetails = new ArrayList<>();
+    private List<CategoryList> catDetails = new ArrayList<>();
     private RecyclerView recyclerView;
-    private MoviesAdapter mAdapter;
-    static View.OnClickListener myOnClickListener;
+    private CategoryAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,76 +25,35 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        mAdapter = new MoviesAdapter(this,catDetails);
+        mAdapter = new CategoryAdapter(this,catDetails);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-       /* mAdapter.setOnItemClickListener(new MoviesAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                Intent intent = new Intent(MainActivity.this,Questions.class);
-                intent.putExtra("catNumber",position);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(),"ureka",Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-        prepareMovieData();
+        prepareCategoryData();
     }
 
-    private void prepareMovieData() {
-        Movie movie = new Movie("Breaking Bad",R.drawable.breakingbadcat);
-        catDetails.add(movie);
+    private void prepareCategoryData() {
+        CategoryList categoryList = new CategoryList("Breaking Bad",R.drawable.breakingbadcat);
+        catDetails.add(categoryList);
 
-        movie = new Movie("Friends",R.drawable.newfriendscat);
-        catDetails.add(movie);
+        categoryList = new CategoryList("Friends",R.drawable.newfriendscat);
+        catDetails.add(categoryList);
 
-        movie = new Movie("Game of thrones",R.drawable.gameofthronescat);
-        catDetails.add(movie);
+        categoryList = new CategoryList("Game of thrones",R.drawable.gameofthronescat);
+        catDetails.add(categoryList);
 
-        movie = new Movie("How i met your mother",R.drawable.newhimymcat);
-        catDetails.add(movie);
+        categoryList = new CategoryList("How i met your mother",R.drawable.newhimymcat);
+        catDetails.add(categoryList);
 
-       /* movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
-        movieList.add(movie);
 
-        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Up", "Animation", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("Star Trek", "Science Fiction", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("The LEGO Movie", "Animation", "2014");
-        movieList.add(movie);
-
-        movie = new Movie("Iron Man", "Action & Adventure", "2008");
-        movieList.add(movie);
-
-        movie = new Movie("Aliens", "Science Fiction", "1986");
-        movieList.add(movie);
-
-        movie = new Movie("Chicken Run", "Animation", "2000");
-        movieList.add(movie);
-
-        movie = new Movie("Back to the Future", "Science Fiction", "1985");
-        movieList.add(movie);
-
-        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
-        movieList.add(movie);
-
-        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
-        movieList.add(movie);
-
-        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
-        movieList.add(movie);*/
 
         mAdapter.notifyDataSetChanged();
     }
 
-
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
 }
--- app/src/main/java/com/example/harikrishnan/task1/Questions.java
+++ app/src/main/java/com/example/harikrishnan/task1/Questions.java
@@ -1,11 +1,16 @@
 package com.example.harikrishnan.task1;
 
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,20 +18,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Questions extends AppCompatActivity {
 
 
-
-     TextView qN,highScore,singleScore;
-     Button choice_1,choice_2,choice_3,choice_4;
-
-     PojoQuestion pojoQuestion;
-     FirebaseDatabase database = FirebaseDatabase.getInstance("https://tvtrivia-9d35d.firebaseio.com/");
-    DatabaseReference reference = database.getReference("01");
+    TextView trivias,score;
+    Button choice_1, choice_2, choice_3, choice_4;
+    ProgressBar progressBar;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    int i=1,scoreValue=0;
+    String answer,showName,qNumber;
+    QBank data;
+    Map<String, String> map;
+    int catPosition;
 
 
     @Override
@@ -34,59 +40,200 @@ public class Questions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
-        qN = findViewById(R.id.question);
-        highScore = findViewById(R.id.highscore);
-        singleScore = findViewById(R.id.score);
-        choice_1 = findViewById(R.id.choice1);
-        choice_2 = findViewById(R.id.choice2);
-        choice_3 = findViewById(R.id.choice3);
+        trivias = findViewById(R.id.question);
+        score = findViewById(R.id.score);
         choice_4 = findViewById(R.id.choice4);
+        choice_3 = findViewById(R.id.choice3);
+        choice_2 = findViewById(R.id.choice2);
+        choice_1 = findViewById(R.id.choice1);
+        progressBar = findViewById(R.id.progressbar);
+        database = FirebaseDatabase.getInstance();
+        data = new QBank();
 
+        Bundle bundle = getIntent().getExtras();
+        catPosition = bundle.getInt("catNumber");
 
-        settingQuestion();
+        getData();
+
 
     }
 
-    private void settingQuestion() {
+    private void getData() {
 
+
+
+
+
+        if (catPosition == 0){
+            showName = "Breakingbad";
+        }
+        else if (catPosition == 1) {
+            showName = "Friends";
+        }
+        else if (catPosition == 2){
+            showName = "Gameofthrones";
+        }
+        else if (catPosition == 3){
+            showName = "Himym";
+        }
+
+        if (i == 1){
+            qNumber = "one";
+
+        }
+        else if (i == 2){
+            qNumber = "two";
+        }
+        else if (i == 3){
+            qNumber = "three";
+
+        }
+        else if (i == 4){
+
+            qNumber = "four";
+        }
+        else if (i == 5){
+
+            qNumber = "five";
+        }
+        else if (i==6){
+            Intent intent = new Intent(Questions.this,Result.class);
+            intent.putExtra("score",scoreValue);
+            startActivity(intent);
+
+        }
+
+        reference = database.getReference().child(showName).child(qNumber);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                for (DataSnapshot children : dataSnapshot.getChildren()) {
+                progressBar.setVisibility(View.INVISIBLE);
+                map = (Map)dataSnapshot.getValue();
+                String c4 = map.get("c4");
+                String c3 = map.get("c3");
+                String c2 = map.get("c2");
+                String c1 = map.get("c1");
+                String ans = map.get("ans");
+                String name = map.get("Name");
 
+                data = new QBank(name,c1,c2,c3,c4,ans);
 
-
-                    String question = children.child("Q").getValue().toString();
-                    String c1 = children.child("Choice1").getValue().toString();
-                    String c2 = children.child("Choice2").getValue().toString();
-                    String c3 = children.child("Choice3").getValue().toString();
-                    String c4 = children.child("Choice4").getValue().toString();
-                    String ans = children.child("Answer").getValue().toString();
-
-
-
-
-                  pojoQuestion = new PojoQuestion(question,c1,c2,c3,c4,ans);
-
-                }
-
-                qN.setText(pojoQuestion.getQname());
-                choice_1.setText(pojoQuestion.getChoice_1());
-                choice_2.setText(pojoQuestion.getChoice_2());
-                choice_3.setText(pojoQuestion.getChoice_3());
-                choice_4.setText(pojoQuestion.getChoice_4());
+                printDataAndClickEvent();
 
 
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+                printDataAndClickEvent();
+
+            }
+        });
+
+
+
+
+    }
+
+    private void printDataAndClickEvent() {
+        trivias.setText(data.getName());
+        choice_1.setText(data.getC1());
+        choice_2.setText(data.getC2());
+        choice_3.setText(data.getC3());
+        choice_4.setText(data.getC4());
+        score.setText(String.valueOf(scoreValue));
+        answer = data.getCorrect();
+
+        choice_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String c1 = choice_1.getText().toString().trim();
+                if (c1.equals(answer)){
+                    Toast.makeText(getApplicationContext(),"correct",Toast.LENGTH_SHORT).show();
+                    i++;
+                    scoreValue = scoreValue + 10;
+                    getData();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"wrong",Toast.LENGTH_SHORT).show();
+                    i++;
+                    getData();
+
+                }
+
+            }
+        });
+        choice_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String c2 = choice_2.getText().toString().trim();
+                if (c2.equals(answer)){
+                    Toast.makeText(getApplicationContext(),"correct",Toast.LENGTH_SHORT).show();
+                    i++;
+                    scoreValue = scoreValue + 10;
+
+                    getData();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"wrong",Toast.LENGTH_SHORT).show();
+                    i++;
+                    getData();
+
+                }
+
+            }
+        }); choice_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String c3 = choice_3.getText().toString().trim();
+                if (c3.equals(answer)){
+                    Toast.makeText(getApplicationContext(),"correct",Toast.LENGTH_SHORT).show();
+                    i++;
+                    scoreValue = scoreValue + 10;
+
+                    getData();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"wrong",Toast.LENGTH_SHORT).show();
+                    i++;
+                    getData();
+
+                }
+
+            }
+        }); choice_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String c4 = choice_4.getText().toString().trim();
+                if (c4.equals(answer)){
+                    Toast.makeText(getApplicationContext(),"correct",Toast.LENGTH_SHORT).show();
+                    i++;
+                    scoreValue = scoreValue + 10;
+
+                    getData();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"wrong",Toast.LENGTH_SHORT).show();
+                    i++;
+                    getData();
+
+                }
 
             }
         });
 
 
     }
+
+
 }
+
+
+
+
+
+
+
